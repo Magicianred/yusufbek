24c5aebf55d9456b390ba201d75448eef5f698f9
Mukhammadkarim Tukhtaboyev <toxtaboyev.m@iCloud.com>
2021-06-01 22:34:23 +0500

    Add project files.

---
+++ yusufbek/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using yusufbek.Models;
+
+namespace yusufbek.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _environment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Music()
+        {
+            string rootPath = _environment.WebRootPath;
+            string fileName = Path.Combine("files", "hamdam.mp3");
+
+            byte[] stream = System.IO.File.ReadAllBytes(Path.Combine(rootPath, fileName));
+            return File(stream, "application/mp3", "music.mp3");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
